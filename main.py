e18cca1972168f51ba67d9849bf65c57b87e33bf
Eryk Orzech <erykorzech05@gmail.com>
2026-05-31 15:45:06 +0200

    test simulation

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from simulation import (
     create_initial_neutrons,
-    create_nuclei
+    create_nuclei,
+    run_transport
 )
 
 neutrons = create_initial_neutrons()
 nuclei = create_nuclei()
 
-print(f"Neutrony: {len(neutrons)}")
-print(f"Jądra: {len(nuclei)}")
+print(f"Start: {len(neutrons)} neutronów")
+
+neutrons = run_transport(neutrons)
+
+print("Koniec symulacji")
