d130ac469911af23de9b7967c219ba49975bac88
Eryk Orzech <erykorzech05@gmail.com>
2026-05-31 15:39:26 +0200

    move_neutron function

--- physics.py
+++ physics.py
@@ -1,10 +1,15 @@
 import numpy as np
-from config import REACTOR_RADIUS
+from config import (
+    REACTOR_RADIUS,
+    MEAN_FREE_PATH
+)
+
 
 def random_unit_vector() -> np.ndarray:
     vector = np.random.normal(size = 3)
 
     return vector / np.linalg.norm(vector)
+
 
 def random_position_in_sphere(radius = REACTOR_RADIUS):
     while True:
@@ -16,3 +21,11 @@ def random_position_in_sphere(radius = REACTOR_RADIUS):
 
         if np.linalg.norm(point) <= radius:
             return point
+        
+
+def move_neutron(position, direction):
+    step = np.random.exponential(MEAN_FREE_PATH)
+
+    return position + direction * step
+
+
