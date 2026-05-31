2614726cb30fed6bbc2d2d4a5f8ff1122ffd7aff
Eryk Orzech <erykorzech05@gmail.com>
2026-05-31 15:29:13 +0200

    add physics

---
+++ physics.py
@@ -0,0 +1,18 @@
+import numpy as np
+from config import REACTOR_RADIUS
+
+def random_unit_vector() -> np.ndarray:
+    vector = np.random.normal(size = 3)
+
+    return vector / np.linalg.norm(vector)
+
+def random_position_in_sphere(radius = REACTOR_RADIUS):
+    while True:
+        point = np.random.uniform(
+            -radius,
+            radius,
+            size = 3
+        )
+
+        if np.linalg.norm(point) <= radius:
+            return point
